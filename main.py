8515023a679790be168453cdfdcdad087d81db1f
Luiza <64799018+LuizaAlanis@users.noreply.github.com>
2023-01-21 23:09:47 -0300

    explaining the code

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
+# imports
 import os
 from pathlib import Path
 import pandas as pd
 import wget
 
-
+# product code = id
 def get_config(product_code):
     df = pd.read_json('config.json')
     result_df = df.loc[df['product_code'] == product_code]
-
+    # json that contains some product informations
     return result_df
 
-
+# upload function
 def upload_file(file, folder):
     file_name = "/ic_logo.png"
     directory = "/home/luiza/Pictures/"
 
     wget.download(file, directory + folder + file_name)
 
-
+# loop for upload icons
 def upload_logos():
     product_config = get_config('A')
     app_logos = product_config['custom_interface'].values[0]['app_icon']
